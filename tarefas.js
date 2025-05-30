381fad88fdbd8264dcd96eaa849f4722a0e1b8a6
Arthur Ferian <arthurferian.17@gmail.com>
2025-05-30 08:55:44 -0300

    Feat: Remove última tarefa

--- tarefas.js
+++ tarefas.js
@@ -1,6 +1,11 @@
+// Atividade 3
 let tarefas = ['Estudar JavaScript', 'Fazer exercícios de matemática', 'Lavar a louça', 'Comprar mercado', 'Ir à academia', 'Ler um livro', 'Fazer revisão para o teste', 'Limpar o quarto', 'Estudar para concurso', 'Organizar o computador'];
 
 tarefas.push(`Jogar`, `Tomar banho`)
-
 console.log(`Com as novas tarefas adicionadas:`, tarefas);
+
+// Atividade 4
+
+let ultimaTarefaRemovida = tarefas.pop()
+console.log(`Com a última tarefa removida: ${tarefas}`);
 
