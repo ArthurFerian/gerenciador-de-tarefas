aa7c5d67fd19ef480023958d55c8a6d101ea2c19
Arthur Ferian <arthurferian.17@gmail.com>
2025-05-30 08:41:22 -0300

    Feat: Adiciona novas tarefas

---
+++ tarefas.js
@@ -0,0 +1,6 @@
+let tarefas = ['Estudar JavaScript', 'Fazer exercícios de matemática', 'Lavar a louça', 'Comprar mercado', 'Ir à academia', 'Ler um livro', 'Fazer revisão para o teste', 'Limpar o quarto', 'Estudar para concurso', 'Organizar o computador'];
+
+tarefas.push(`Jogar`, `Tomar banho`)
+
+console.log(`Com as novas tarefas adicionadas:`, tarefas);
+
