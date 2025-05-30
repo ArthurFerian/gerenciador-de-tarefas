e898263a991017320bb15f1651fedcf9efd17199
Arthur Ferian <arthurferian.17@gmail.com>
2025-05-30 09:07:57 -0300

    Feat: Adiciona tarefa no início

--- tarefas.js
+++ tarefas.js
@@ -1,11 +1,19 @@
-// Atividade 3
+// Etapa 1
 let tarefas = ['Estudar JavaScript', 'Fazer exercícios de matemática', 'Lavar a louça', 'Comprar mercado', 'Ir à academia', 'Ler um livro', 'Fazer revisão para o teste', 'Limpar o quarto', 'Estudar para concurso', 'Organizar o computador'];
 
 tarefas.push(`Jogar`, `Tomar banho`)
 console.log(`Com as novas tarefas adicionadas:`, tarefas);
 
-// Atividade 4
+// Etapa 2
 
 let ultimaTarefaRemovida = tarefas.pop()
-console.log(`Com a última tarefa removida: ${tarefas}`);
+console.log(`Com a última tarefa removida: ${tarefas}`)
+
+// Etapa 3
+
+tarefas.unshift(`Tomar banho`)
+let addTarefaNoInicio = tarefas.unshift()
+
+console.log(`Com a nova tarefa adicionada no ínico: ${tarefas}`);
+
 
