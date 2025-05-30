86ad72dd1174a5a647c0a5b2bb8c9693e18143bc
Arthur Ferian <arthurferian.17@gmail.com>
2025-05-30 10:04:21 -0300

    Feat: Remove primeira tarefa

--- tarefas.js
+++ tarefas.js
@@ -1,19 +1,22 @@
 // Etapa 1
 let tarefas = ['Estudar JavaScript', 'Fazer exercícios de matemática', 'Lavar a louça', 'Comprar mercado', 'Ir à academia', 'Ler um livro', 'Fazer revisão para o teste', 'Limpar o quarto', 'Estudar para concurso', 'Organizar o computador'];
 
-tarefas.push(`Jogar`, `Tomar banho`)
+tarefas.push(`Jogar`, `Tomar banho`);
 console.log(`Com as novas tarefas adicionadas:`, tarefas);
 
 // Etapa 2
 
-let ultimaTarefaRemovida = tarefas.pop()
-console.log(`Com a última tarefa removida: ${tarefas}`)
+let ultimaTarefaRemovida = tarefas.pop();
+console.log(`Com a última tarefa removida: ${tarefas}`);
 
 // Etapa 3
 
-tarefas.unshift(`Tomar banho`)
-let addTarefaNoInicio = tarefas.unshift()
+tarefas.unshift(`Tomar banho`);
+let addTarefaNoInicio = tarefas.unshift();
 
 console.log(`Com a nova tarefa adicionada no ínico: ${tarefas}`);
 
+// Etapa 4
 
+let primeiraTarefaRemovida = tarefas.shift();
+console.log(`Com a primeira tarefa removida: ${tarefas}`)
